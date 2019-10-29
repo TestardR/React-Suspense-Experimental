9fd0dade351ad1ce4516e803d7efc2e317bea407
rtestard <rtestard@opensense.fr>
2019-10-29 17:10:01 +0100

    refreshing data

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import { createResource } from './PersonAPI';
 import Person from './Person';
 import Num from './Num';
 
 
-const resource = createResource();
+const initialResource = createResource();
 
 function App() {
+ /*  const [resource, setResource] = useState(() => createResource()); */
+ const [resource, setResource] = useState(initialResource);
+
   return (
     <div className="App">
-      <Suspense fallback={<h1>Loading...</h1>}>
-        <Person resource={resource}></Person>
+      <Suspense fallback={<h1>Loading num...</h1>}>
         <Num resource={resource}></Num>
       </Suspense>
+      <Suspense fallback={<h1>Loading person...</h1>}>
+        <Person resource={resource}></Person>
+      </Suspense>
+      <button onClick={() => {
+        setResource(createResource())
+      }}>Refresh Data</button>
     </div>
   );
 }
