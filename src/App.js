eba3e798af6f7e38070037a71b1ebfe67098e1da
rtestard <rtestard@opensense.fr>
2019-10-29 17:15:12 +0100

    Using ErroBoundary

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Suspense, useState } from 'react';
 import { createResource } from './PersonAPI';
 import Person from './Person';
 import Num from './Num';
+import { ErrorBoundary } from './ErrorBoundary';
 
 
 const initialResource = createResource();
@@ -12,13 +13,15 @@ function App() {
 
   return (
     <div className="App">
+    <ErrorBoundary>
       <Suspense fallback={<h1>Loading num...</h1>}>
         <Num resource={resource}></Num>
       </Suspense>
       <Suspense fallback={<h1>Loading person...</h1>}>
         <Person resource={resource}></Person>
       </Suspense>
-      <button onClick={() => {
+    </ErrorBoundary>
+    <button onClick={() => {
         setResource(createResource())
       }}>Refresh Data</button>
     </div>
